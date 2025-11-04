6307b18aed23386b4cb064a7344a46a77f3d0eb5
remnantcjy <remnantcjy@gmail.com>
2025-11-04 11:46:34 +0900

    Initial commit

---
+++ src/test/java/org/example/schedulemanager/ScheduleManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.schedulemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
