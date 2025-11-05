c1a8fd7ca9929e2403637907601e811278941662
remnantcjy <remnantcjy@gmail.com>
2025-11-05 16:34:11 +0900

    refactor: Controller 응답 형식 ResponseEntity로 통일

--- src/main/java/org/example/schedulemanager/controller/ScheduleController.java
+++ src/main/java/org/example/schedulemanager/controller/ScheduleController.java
@@ -3,6 +3,8 @@ package org.example.schedulemanager.controller;
 import lombok.RequiredArgsConstructor;
 import org.example.schedulemanager.dto.*;
 import org.example.schedulemanager.service.ScheduleService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,51 +18,53 @@ public class ScheduleController {
 
     // Lv 1. 일정 생성
     @PostMapping("/schedules")
-    public CreateScheduleResponse createSchedule(@RequestBody CreateScheduleRequest request) {
+    public ResponseEntity<CreateScheduleResponse> createSchedule(@RequestBody CreateScheduleRequest request) {
 
         CreateScheduleResponse result = scheduleService.save(request);
 
-        return result;
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
     }
 
     // Lv 2. 일정 조회
     // 전체 일정 조회 - 작성자명 기준으로 조회도 가능
     @GetMapping("/schedules")
-    public List<GetScheduleResponse> getSchedules(@RequestParam(required = false) String name) {
+    public ResponseEntity<List<GetScheduleResponse>> getSchedules(@RequestParam(required = false) String name) {
 
         List<GetScheduleResponse> result = scheduleService.findAll(name);
 
-        return result;
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     // 선택 일정 조회 - id로 식별
     @GetMapping("/schedules/{id}")
-    public GetScheduleResponse getOneSchedule(@PathVariable Long id) {
+    public ResponseEntity<GetScheduleResponse> getOneSchedule(@PathVariable Long id) {
 
         GetScheduleResponse result = scheduleService.getOne(id);
 
-        return result;
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     // Lv 3. 일정 수정
     @PutMapping("/schedules/{id}")
-    public UpdateScheduleResponse updateSchedule(
+    public ResponseEntity<UpdateScheduleResponse> updateSchedule(
             @PathVariable Long id,
             @RequestBody UpdateScheduleRequest request
     ) {
 
         UpdateScheduleResponse result = scheduleService.update(id, request);
 
-        return result;
+        return ResponseEntity.status(HttpStatus.OK).body(result);
     }
 
     // Lv 4. 일정 삭제
     @DeleteMapping("/schedules/{id}")
-    public void delete(
+    public ResponseEntity<Void> delete(
             @PathVariable Long id,
             @RequestBody DeleteScheduleRequest request
     ) {
         scheduleService.delete(id, request);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
 }
